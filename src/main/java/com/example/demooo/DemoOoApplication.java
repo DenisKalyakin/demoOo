157c8832b2ff715d77b404bcdb3aea54e712c59f
Denis Kalyakin <denis.kalyakin999@gmail.com>
2024-04-24 21:09:54 +0300

    first commit

---
+++ src/main/java/com/example/demooo/DemoOoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demooo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoOoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoOoApplication.class, args);
+    }
+
+}
